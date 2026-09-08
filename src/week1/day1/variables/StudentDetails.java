1659257f0602980b512e09b38981dbf90163f905
Rama <kesanaramadevi482@gmail.com>
2026-09-08 13:44:31 -0400

    Week 1 Day 1: Practice variables and data types

---
+++ src/week1/day1/variables/StudentDetails.java
@@ -0,0 +1,17 @@
+package week1.day1.variables;
+public class StudentDetails {
+
+    public static void main(String[] args) {
+
+        String studentName = "John Doe";
+        int studentAge = 24;
+        double studentGpa = 3.8;
+        char studentGrade = 'A';
+        boolean isActive = true;
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Gpa: " + studentGpa);
+        System.out.println("Student Grade: " + studentGrade);
+        System.out.println("Student isActive: " + isActive);
+    }
+}
