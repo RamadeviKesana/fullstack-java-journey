78f947312b737a06e5cb0ef2acb4ce7ef798c7d0
Rama <kesanaramadevi482@gmail.com>
2026-09-08 11:23:46 -0400

    Week 1 Day 1: Add Hello Java program

---
+++ src/week1/day1/hello/HelloJava.java
@@ -0,0 +1,7 @@
+package week1.day1.hello;
+
+public class HelloJava { //class name should be same as file name
+    public static void main(String[] args) { //main() method
+        System.out.println("Welcome to my Java Full Stack Journey!");
+    }
+}
